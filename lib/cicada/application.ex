f27647a96ae8ad179b6731e8f30dd2854ea0fd6c
Christopher Coté <Christopher Cot>
2017-05-03 11:26:04 -0500

    disable voice control

--- lib/cicada/application.ex
+++ lib/cicada/application.ex
@@ -1,6 +1,6 @@
 defmodule Cicada.Application do
   use Application
-  alias Cicada.{API, NetworkManager, SysMon, DataManager, DeviceManager, EventManager, DistributionManager, Util, VoiceControl}
+  alias Cicada.{API, NetworkManager, SysMon, DataManager, DeviceManager, EventManager, DistributionManager, Util}#, VoiceControl}
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
@@ -11,7 +11,7 @@ defmodule Cicada.Application do
       supervisor(DistributionManager.Supervisor, []),
       supervisor(SysMon.Supervisor, []),
       supervisor(API.Supervisor, []),
-      supervisor(VoiceControl.Supervisor, []),
+      #supervisor(VoiceControl.Supervisor, []),
     ]
 
     opts = [strategy: :one_for_one, name: Cicada.Supervisor]
